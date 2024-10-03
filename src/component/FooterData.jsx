6f38867e7f79c105ca76e982f4cd7459a3fa1662
Sanjay Mehta <sanjay.mehta@hidubai.com>
2024-10-03 17:49:32 +0400

    San Consulting project

---
+++ src/component/FooterData.jsx
@@ -0,0 +1,30 @@
+import { faMobileAlt, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { faBuffer } from '@fortawesome/free-brands-svg-icons'
+
+export const usefulLink = [
+    {name: 'Home', id: 1},
+    {name: 'About us', id: 2},
+    {name: 'Services', id: 3},
+    {name: 'Team', id: 4},
+    {name: 'Blog', id: 5}
+]
+export const ourServices = [
+    {name: 'Strategy & Research', id: 6},
+    {name: 'Web Design', id: 7},
+    {name: 'Web Development', id: 8},
+    {name: 'Digital Marketing', id: 9},
+    {name: 'Graphic Design', id: 10}
+]
+export const otherLinks = [
+    {name: 'FAQ', id: 11},
+    {name: 'Portfolio', id: 12},
+    {name: 'Privacy Policy', id: 13},
+    {name: 'Terms & Conditions', id: 14},
+    {name: 'Support', id: 15},
+]
+
+export const footerInfo = [
+    {icon: faBuffer, info1: 'SAN CONSULTING', id: 1},
+    {icon: faMobileAlt, info1: '+971 506344397', info2: 'sanjay.mehta1995@gmail.com', id: 2},
+    {icon: faMapMarkedAlt, info1: 'Dubai, United Arab Emirates', info2: 'Delhi,India', id: 3}
+];
